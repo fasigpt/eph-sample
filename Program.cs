421f9d7408fbe2c5075cf3e8691afe405cb1c785
Mohammed Fasiulla <mofasiul@microsoft.com>
2020-01-30 10:47:01 -0800

    fff

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+
+namespace ReceiveEventsFromEventHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "<EH Connection String>";
+            string eventHubName = "<entityName>";
+            string storageConnectionString = "<storage Connection String>";
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, connectionString, storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            var options = new EventProcessorOptions();
+            options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            eventProcessorHost.RegisterEventProcessorAsync<EventProcessor>(options).Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
